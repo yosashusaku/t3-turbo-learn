d4d9d5ceca6eb1234aabda5a76ad6254d16a1876
与謝秀作 <yosa.shusaku@ficilcom.jp>
2024-12-27 23:09:50 +0900

    trpc panel

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TRPCModule } from 'nestjs-trpc';
+import { TrpcPanelController } from './panel/trpc-panel.controller';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TRPCModule.forRoot({
+      autoSchemaFile: './src/@generated',
+    }),
+  ],
+  controllers: [AppController, TrpcPanelController],
   providers: [AppService],
 })
 export class AppModule {}
