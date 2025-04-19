f3c8cda6421b506dbac3cdef649574315ee91753
与謝秀作 <yosa.shusaku@ficilcom.jp>
2025-04-19 21:35:13 +0900

    from eslint to biome

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
-import { TRPCModule } from 'nestjs-trpc';
+import { TRPCModule } from "nestjs-trpc";
 
 @Module({
   imports: [
     TRPCModule.forRoot({
-      autoSchemaFile: './src/@generated',
+      autoSchemaFile: "./src/@generated",
     }),
   ],
   controllers: [AppController],
